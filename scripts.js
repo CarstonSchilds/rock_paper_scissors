0f6dc5b2947934f42bb354c67914eda7c77a382e
Origier <sergeantbeast456@gmail.com>
2018-11-08 04:35:00 -0500

    recursive function works until end of game

--- scripts.js
+++ scripts.js
@@ -1,27 +1,37 @@
 const startButton = document.querySelector('#start');
 const roundsInput = document.querySelector('#number-of-rounds');
+const buttonsDiv = document.querySelector('#buttons');
 const buttons = document.querySelectorAll('#buttons > .button');
+const formDiv = document.querySelector('#user-entry');
+const form = document.querySelector('#user-entry > form');
+let madeSelection = true;
 let gameRounds;
 let playerSelection;
 let computerSelection;
+let result;
 let timeOut;
+let playerScore;
+let computerScore;
+let i = 0;
 
 startButton.addEventListener('click', checkStartConditions);
 
-function playGame(rounds) {
-   for(let i = 0; i < rounds; i++){
-      playerSelection = '';
-      computerSelection = '';
-      buttons.forEach(button => {
-         button.addEventListener('click', playStart)
-      });
-
-      startTimeOut();
-   }
+function playGame() {
+   i = 0;
+   playerScore = 0;
+   computerScore = 0;
+   removeForm();
+   buttons.forEach(button => {
+      button.addEventListener('click', playStart)
+   });
+   
+   startTimeOut();
 }
 
 function playStart(e) {
+   madeSelection = true;
    playerSelection = e.target.id;
+   endTimeOut();
    computerPlay();
 }
 
@@ -30,8 +40,16 @@ function checkStartConditions() {
    if(isNaN(gameRounds)) {
       alert('You didn\'t enter an integer!');
    } else {
-      playGame(gameRounds);
+      playGame();
    }
+}
+
+function removeForm() {
+   formDiv.removeChild(form);
+}
+
+function addForm() {
+   formDiv.insertBefore(form, buttonsDiv);
 }
 
 function endTimeOut() {
@@ -39,12 +57,41 @@ function endTimeOut() {
 }
 
 function startTimeOut() {
-   timeOut = setTimeout(queryTheUsersPresence, 20000);
+   if(i < gameRounds && madeSelection == true) {
+      playerSelection = '';
+      computerSelection = '';
+      result = '';
+      i += 1;
+      timeOut = setTimeout(queryTheUsersPresence, 20000);
+   } else if (i == gameRounds) {
+      endTheGame();
+   } else {
+      timeOut = setTimeout(queryTheUsersPresence, 20000);
+   }
 }
 
 function queryTheUsersPresence() {
    alert('Are you still there?');
+   madeSelection = false;
    startTimeOut();
+}
+
+function seeWhoWins() {
+   if(playerSelection == computerSelection) {
+      result = 'tie';
+   } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+      result = 'win';
+   } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+      result = 'lose';
+   } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+      result = 'lose';
+   } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+      result = 'win';
+   } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+      result = 'lose';
+   } else {
+      result = 'win';
+   }
 }
 
 function computerPlay() {
@@ -59,4 +106,5 @@ function computerPlay() {
       case 3:
       computerSelection = 'scissors';
    }
+   seeWhoWins();
 }
